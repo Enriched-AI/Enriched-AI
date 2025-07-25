b08850b528ae2ead7d8372192073eb5d295004c7
Enriched-AI <enriched.aiml@gmail.com>
2025-07-25 13:11:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,12 +19,7 @@ async def chat_endpoint():
             # hyper_browser_api_key=os.getenv('HYPER_BROWSER_API_KEY')
            
         )
-        # chatbot = ChatBot(
-            
-        #     weather_api_key=os.getenv("WEATHER_API"),google_places_api_key=os.getenv("GOOGLE_PLACES_API_KEY"),mongodb_link=os.getenv("MONGODB"), publisher_id=os.getenv('PARTNERIZE_PUBLISHER_ID'),APPLICATION_KEY=os.getenv('PARTNERIZE_APPLICATION_KEY'),USER_API_KEY=os.getenv('PARTNERIZE_USER_API_KEY'),
-        #     hyper_browser_api_key=os.getenv('HYPER_BROWSER_API_KEY')
-           
-        # )
+        
         data  = request.get_json()
         user_input = data.get("user_input", "")
        
@@ -42,6 +37,41 @@ async def chat_endpoint():
             "error": str(e),
             "history": []
         })
+
+
+@app.route('/llm/chat/pocruise',methods=['POST'])
+async def chat_endpoint():
+    """
+    FastAPI endpoint to process a single user input and return chatbot response.
+    """
+    try:
+       
+    
+        chatbot = ChatBot(
+            
+            weather_api_key=os.getenv("WEATHER_API"),google_places_api_key=os.getenv("GOOGLE_PLACES_API_KEY"),mongodb_link=os.getenv("MONGODB"), publisher_id=os.getenv('PARTNERIZE_PUBLISHER_ID'),APPLICATION_KEY=os.getenv('PARTNERIZE_APPLICATION_KEY'),USER_API_KEY=os.getenv('PARTNERIZE_USER_API_KEY'),
+            hyper_browser_api_key=os.getenv('HYPER_BROWSER_API_KEY')
+           
+        )
+        data  = request.get_json()
+        user_input = data.get("user_input", "")
+       
+        # Get response
+        response = await chatbot.chat2(user_input)
+   
+        return jsonify({
+            "response": response,
+            
+        })
+
+    except Exception as e:
+
+        return jsonify({
+            "error": str(e),
+            "history": []
+        })
+
+
 
 
 if __name__ == '__main__':
